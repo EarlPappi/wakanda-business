4e0ba6e2bcd27ca57ef9185d9c8060aec3344422
earlpappi <earlpappi@gmail.com>
2022-08-30 20:18:23 +0100

    footer completed?

---
+++ src/Components/Sections/PreFooter/Prefooter.jsx
@@ -0,0 +1,14 @@
+import Wrapper from "../../Containers/Wrapper/Wrapper";
+
+
+const Prefooter = () => {
+    return ( 
+        <div>
+            <Wrapper>
+                <h1>Prefooter starts here</h1>
+            </Wrapper>
+        </div>
+     );
+}
+ 
+export default Prefooter;
