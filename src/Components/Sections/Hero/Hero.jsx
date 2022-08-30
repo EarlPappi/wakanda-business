ed616bc292a4e06d739bf075033be81e85aab6a9
earlpappi <earlpappi@gmail.com>
2022-08-30 21:24:39 +0100

    hero section started

--- src/Components/Sections/Hero/Hero.jsx
+++ src/Components/Sections/Hero/Hero.jsx
@@ -1,9 +1,25 @@
+import Button from "../../Containers/Button/Button";
+import Wrapper from "../../Containers/Wrapper/Wrapper";
+import HeroImg from "../../../Assets/Mask Group 15.png";
+import classes from "./Hero.module.css"
 
 
 const Hero = () => {
     return ( 
-        <div>
-            <h1>This is the Hero</h1>
+        <div className={ classes.heroCon }>
+            <Wrapper>
+                <div className={ classes.heroTopSection }>
+                    <div className={ classes.heroTextCon }>
+                        <h2>Organise Your Sales. Build Great Relationships. Save Time.</h2>
+
+                        <Button>Get Started</Button>
+                    </div>
+
+                    <div className={ classes.heroImgCon }>
+                        <img src={ HeroImg } alt="" width="100%"/>
+                    </div>
+                </div>
+            </Wrapper>
         </div>
      );
 }
--- src/Components/Sections/PreFooter/Prefooter.jsx
+++ src/Components/Sections/PreFooter/Prefooter.jsx
@@ -1,14 +1,26 @@
 import Wrapper from "../../Containers/Wrapper/Wrapper";
+import classes from "./Prefooter.module.css"
+import Book from "../../../Assets/Mask Group 17.png"
 
 
 const Prefooter = () => {
-    return ( 
+    return (
         <div>
             <Wrapper>
-                <h1>Prefooter starts here</h1>
+                <div className={ classes.preFooterIn }>
+                    <div>
+                        <img src={Book} alt="" width="100%"/>
+
+                    </div>
+
+                    <div className={ classes.texBox }>
+                        <h2>FREE! EBOOK</h2>
+                        <p>7 Reasons You Need To Turn Your Business To A Mobile Business</p>
+                    </div>
+                </div>
             </Wrapper>
         </div>
-     );
+    );
 }
- 
+
 export default Prefooter;
