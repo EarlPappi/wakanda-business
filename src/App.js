b75a3645f877799c0ede4e0c351130e1628f7bb2
earlpappi <earlpappi@gmail.com>
2022-08-31 23:19:47 +0100

    mobile view

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import Footer from "./Components/Footer/Footer";
 import Home from "./Components/Home";
-import Features from "./Components/Sections/Features/Features";
-import Hero from "./Components/Sections/Hero/Hero";
-import Prefooter from "./Components/Sections/PreFooter/Prefooter";
 
 function App() {
   return (
     <div>
       <Home/>
-      <Hero/>
-      <Features/>
-      <Prefooter/>
-      <Footer/>
     </div>
   )
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,19 @@
 import Header from "./Header/Header";
+import Hero from "../Components/Sections/Hero/Hero"
+import Features from "../Components/Sections/Features/Features"
+import Prefooter from "../Components/Sections/PreFooter/Prefooter"
+import Footer from "../Components/Footer/Footer"
 
 const Home = () => {
-    return ( 
+    return (
         <div>
-            <Header/>
+            {/* <Header /> */}
+            <Hero />
+            <Features />
+            {/* <Prefooter /> */}
+            {/* <Footer /> */}
         </div>
-     );
+    );
 }
- 
+
 export default Home;
